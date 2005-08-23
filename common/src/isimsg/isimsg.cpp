cf80a8088c25c8c387b94ee52957ea52f526e7d5
z-g <z-g@28b348bd-d910-0410-8a2e-dc5dcabf243e>
2005-08-23 16:39:09 +0000

    include fix

--- common/src/isimsg/isimsg.cpp
+++ common/src/isimsg/isimsg.cpp
@@ -17,7 +17,7 @@
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 */
 
-#include "isimsg.hpp"
+#include <isimsg.hpp>
 
 GLDEF_C TInt E32Dll(TDllReason /*aReason*/)
 {
--- common/src/zg_vibra/zg_vibra.cpp
+++ common/src/zg_vibra/zg_vibra.cpp
@@ -17,7 +17,7 @@
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 */
 
-#include "zg_vibra.hpp"
+#include <zg_vibra.hpp>
 
 CVibraReq* CVibraReq::NewL(TUint8 aParam,TUint8 aState) //checked
 {
