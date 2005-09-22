919872b681b3c25e8897a6af8bc6159ffeca0b3a
z-g <z-g@28b348bd-d910-0410-8a2e-dc5dcabf243e>
2005-09-22 12:18:16 +0000

    build incremented.

--- trunk/tricks/src/ngage/hwother.cpp
+++ trunk/tricks/src/ngage/hwother.cpp
@@ -25,7 +25,7 @@ EXPORT_C void HWOther::InfoL(TInfoType aType,TInt& aValue)
   switch(aType)
   {
     case EInfoBuild:
-      aValue=4;
+      aValue=5;
       break;
     case EInfoServerSupported:
       aValue=ETrue;
