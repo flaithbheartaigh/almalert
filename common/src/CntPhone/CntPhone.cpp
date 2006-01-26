675d77b1c3b61fb154f6c6a7fcb871227b7c626f
z-g <z-g@28b348bd-d910-0410-8a2e-dc5dcabf243e>
2006-01-26 10:28:27 +0000

    CntPhone added.

---
+++ common/src/CntPhone/CntPhone.cpp
@@ -0,0 +1,87 @@
+/*
+    CntPhone.cpp
+    Copyright (C) 2005-2006 zg
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+*/
+
+#include "CntPhone.hpp"
+
+GLDEF_C TInt E32Dll(TDllReason /*aReason*/)
+{
+  return KErrNone;
+}
+
+EXPORT_C CContactPhoneNumberParser* NewPhoneNumberParser(void)
+{
+  return new(ELeave)CContactPhoneNumberParserImpl;
+}
+
+void CContactPhoneNumberParserImpl::ExtractRawNumber(const TDesC& aTextualNumber,TDes& aRawNumber)
+{
+  aRawNumber.Zero();
+  TInt length=aTextualNumber.Length();
+  if(aTextualNumber.Length())
+  {
+    TPtrC ptr(aTextualNumber);
+    TUint16 phChar=ptr[0];
+    if(phChar=='*'||phChar=='#')
+    {
+      TInt plusPosition=ptr.Locate('+');
+      if(plusPosition>4||plusPosition==KErrNotFound)
+      {
+        TInt astericPosition=ptr.LocateReverse('*');
+        TInt barsPosition=ptr.LocateReverse('#');
+        if(astericPosition>=barsPosition)
+        {
+          barsPosition=astericPosition;
+        }
+        plusPosition=barsPosition;
+      }
+      length-=plusPosition;
+      length--;
+      if(length<=0) return;
+      ptr.Set(ptr.Right(length));
+      phChar=ptr[0];
+    }
+    if(phChar=='+')
+    {
+      length--;
+      ptr.Set(ptr.Right(length));
+    }
+    if(!length) return;
+    TLex lex(ptr);
+    for(;;)
+    {
+      TChar currChar=lex.Peek();
+      if(!currChar) break;
+      if(currChar.IsDigit())
+      {
+        aRawNumber.Append(currChar);
+      }
+      else
+      {
+        if(currChar=='*'||currChar=='#')
+        {
+          aRawNumber.Zero();
+          break;
+        }
+        currChar=User::LowerCase(currChar);
+        if(currChar=='p'||currChar=='w'||currChar=='+') break;
+      }
+      lex.Inc();
+    }
+  }
+}
