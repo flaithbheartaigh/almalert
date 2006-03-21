82fbd32deb4c67778f49bb05b1b1461857091e15
z-g <z-g@28b348bd-d910-0410-8a2e-dc5dcabf243e>
2006-03-21 21:41:43 +0000

    DPlatChunkHw added.

---
+++ trunk/common/src/ekern/ekern_dplatchunkhw.cpp
@@ -0,0 +1,48 @@
+/*
+    ekern_dplatchunkhw.cpp
+    Copyright (C) 2006 zg
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+*/
+
+class TProcessMemoryInfo; //don't exists in symbian 6.1
+#include <m32std.h>
+
+EXPORT_C DPlatChunkHw::DPlatChunkHw()
+{
+}
+
+EXPORT_C DPlatChunkHw::~DPlatChunkHw() //FIXME: NOT IMPLEMENTED
+{
+}
+
+EXPORT_C void DPlatChunkHw::DoCreateL(TPhyAddr anAddr,TInt aSize,TChunkType aChunkType) //FIXME: NOT IMPLEMENTED
+{
+}
+
+EXPORT_C void DPlatChunkHw::Read(TInt aPos,TAny *aPtr,TInt aLength) const
+{
+  Mem::Copy(aPtr,(TAny*)(iLinAddr+aPos),aLength);
+}
+
+EXPORT_C void DPlatChunkHw::Write(TInt aPos,const TAny *aPtr,TInt aLength)
+{
+  Mem::Copy((TAny*)(iLinAddr+aPos),aPtr,aLength);
+}
+
+EXPORT_C TLinAddr DPlatChunkHw::LinearAddress(void)
+{
+  return iLinAddr;
+}
