63b320d504ed055ff7ba611c177f749eb94d397a
z-g <z-g@28b348bd-d910-0410-8a2e-dc5dcabf243e>
2006-10-07 10:45:55 +0000

    initial import.

---
+++ clockapp/inc/clockapp.hpp
@@ -0,0 +1,190 @@
+/*
+    clockapp.hpp
+    Copyright (C) 2005-2006 zg
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+*/
+
+#ifndef __CLOCKAPP_HPP__
+#define __CLOCKAPP_HPP__
+
+#include <aknapp.h>
+#include <akndoc.h>
+#include <aknviewappui.h>
+#include <aknview.h>
+#include <ClkWldAlmMdl.hpp>
+#include <aknnavi.h>
+#include <akntitle.h>
+#include <clkdatetimeview.h>
+#include <eikclock.h>
+#include <eikimage.h>
+
+class CClkApplication: public CAknApplication
+{
+  public: //CAknApplication
+    TUid AppDllUid() const;
+  protected: //CAknApplication
+    CApaDocument* CreateDocumentL();
+};
+
+class CClkDocument: public CAknDocument
+{
+  public:
+    static CClkDocument* NewL(CEikApplication& aApp);
+    ~CClkDocument();
+  public: //from CAknDocument
+    CEikAppUi* CreateAppUiL();
+  public:
+    inline CClkAlmModel* Alm(void) {return iAlm;};
+    inline CClkNitzModel* Nitz(void) {return iNitz;};
+    TTime GetLastAlarmTime(void);
+    void SetLastAlarmTime(TTime& anAlarm);
+    void RestoreDataL(void);
+    void StoreDataL(void);
+  private:
+    CClkDocument(CEikApplication& aApp);
+    void ConstructL(void);
+  private:
+    CClkAlmModel* iAlm; //0x24
+    CClkNitzModel* iNitz; //0x28
+    TInt64 iDate; //0x2c
+};
+
+enum TClkEnvChanged
+{
+  EClkEnvChanged3=3
+};
+
+class CClkAppUi: public CAknViewAppUi
+{
+  public:
+    ~CClkAppUi();
+  public:
+    void ShowTitlePaneL(TInt aResId);
+    void CmdSettingsL(void);
+    void CmdHelpL(void);
+    void CmdExit(void);
+  private: //CAknAppUi
+    void HandleCommandL(TInt aCommand);
+    void ConstructL(void);
+  private:
+    static TInt DoNotiferCallbackL(TAny* anAppUi);
+    void NotifyEnvChangedL(TClkEnvChanged anEvent);
+  private:
+    CEnvironmentChangeNotifier* iEnvNotifier; //0x7c
+    CAknNavigationControlContainer* iNaviPane; //0x80
+    CAknTitlePane* iTitlePane; //0x84
+};
+
+const TUid KViewId={1};
+const TUid KClkAlmViewId={3};
+const TUid KSettingsId={10};
+
+class CClkAlmViewContainer;
+
+class CClkAlmView: public CAknView,public MClkModelObserver
+{
+  public:
+    static CClkAlmView* NewLC(CClkAlmModel* anAlm,CClkNitzModel* aNitz);
+    ~CClkAlmView();
+  public:
+    void SetOpenedByOk(TBool aOpenedByOk);
+    TInt NitzState(void);
+    void HandleSettingsChangeL(TClkEnvChanged anEvent);
+  private:
+    void ConstructL(CClkAlmModel* anAlm,CClkNitzModel* aNitz);
+  public: //CAknView
+    TUid Id(void) const;
+    void HandleCommandL(TInt aCommand);
+  public: //MEikMenuObserver
+    void DynInitMenuPaneL(TInt aResourceId,CEikMenuPane* aMenuPane);
+  private: //CAknView
+    void DoActivateL(const TVwsViewId& aPrevViewId,TUid aCustomMessageId,const TDesC8& aCustomMessage);
+    void DoDeactivate(void);
+  private: //MClkModelObserver
+    void HandleUpdateL(TInt aNotification);
+  private:
+    TBool OpenedByOk(void) const;
+    CClkNitzModel* NitzMdl(void);
+    TBool IsSnoozedAlarm(void);
+    void ShowTitlePaneL(void);
+    TBool UpdateAlarmStateL(TInt aParam);
+    TBool UpdateNitzStateL(TInt aParam);
+    void QueryAlarmTimeL(void);
+    void HandleDeleteCmdL(TInt aParam1,TInt aParam2);
+    void ShowRemainTimeL(const TTime& anAlarmTime);
+  private:
+    CClkAlmViewContainer* iControl; //0x38
+    CClkAlmModel* iAlm; //0x3c
+    CClkAlmIdleObserver* iAlmObserver; //0x40
+    CClkNitzModel* iNitz; //0x44
+    CClkNitzIdleObserver* iNitzObserver; //0x48
+    SClockAlarmInfo iAlarmInfo; //0x4c
+    TBool iOpenedByOk; //0x1ac
+    TInt iNitzSettings; //0x1b0
+    TInt iSnoozed; //0x1b4
+};
+
+class CClkAlmViewContainer: public CCoeControl
+{
+  public:
+    static CClkAlmViewContainer* NewL(const TRect& aRect,CClkAlmView& aView);
+    ~CClkAlmViewContainer();
+    void ShowSnoozedAlarmL(const TTime& anAlarm);
+    void ShowActiveAlarmL(const TTime& anAlarm);
+    void ShowInactiveAlarm(void);
+    void SetNitzInfoL(void);
+  public: //CCoeControl
+    TKeyResponse OfferKeyEventL(const TKeyEvent& aKeyEvent,TEventCode aType);
+    void GetHelpContext(TCoeHelpContext& aContext) const;
+    void Draw(const TRect& aRect) const;
+    TInt CountComponentControls() const;
+    CCoeControl* ComponentControl(TInt aIndex) const;
+    void SizeChanged(void);
+  private:
+    void ConstructL(const TRect& aRect,CClkAlmView& aView);
+    void SetAlarmTimeL(const TTime& anAlarm);
+  private:
+    enum TViewType
+    {
+      ETypeActive,
+      ETypeInactive,
+      ETypeSnoozed
+    };
+  private:
+    CEikClock* iClock; //0x30
+    CClkAlmView* iView; //0x34
+    TAknLayoutRect iRect1; //0x38
+    TBuf<5> iAlarmTimeStr; //0x4c
+    TBuf<5> iString2; //0x60
+    TBuf<32> iAlarmWeekStr; //0x74
+    TBuf<12> iAlarmDateStr; //0xbc
+    CEikImage* iImage; //0xdc
+    TViewType iViewType; //0xe0
+    HBufC16* iCurrentTimeStr; //0xe4
+    HBufC16* iLocalTimeStr; //0xe8
+    HBufC16* iNoAlarmsSetStr; //0xec
+    HBufC16* iAlarmStr; //0xf0
+    HBufC16* iSnoozeActiveStr; //0xf4
+    TAknLayoutRect iRect2; //0xf8
+    TBuf<12> iLocalDateStr; //0x10c
+    TBuf<64> iString6; //0x12c
+    TBuf<32> iLocalWeekStr; //0x1b4
+};
+
+#define iClockDocument ((CClkDocument*)Document())
+#define iClkAppUi ((CClkAppUi*)AppUi())
+
+#endif
---
+++ clockapp/src/clockappview.cpp
@@ -0,0 +1,371 @@
+/*
+    clockappview.cpp
+    Copyright (C) 2006 zg
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+*/
+
+#include <aknquerydialog.h>
+#include <stringloader.h> //StringLoader
+#include <aknnotewrappers.h> //CAknConfirmationNote
+#include "clockapp.hpp"
+
+/*#include <hwtricks.hpp> //FIXME: debug
+#include <f32file.h>
+static void Log(const TDesC8& aBuffer,TInt aParam)
+{
+  RFs fs;
+  RFile file;
+  fs.Connect();
+  TInt err=file.Open(fs,_L("e:\\zg_netmon_log.txt"),EFileWrite|EFileShareAny);
+  if(err==KErrNotFound) err=file.Create(fs,_L("e:\\zg_netmon_log.txt"),EFileWrite|EFileShareAny);
+  TInt pos=0;
+  err=file.Seek(ESeekEnd,pos);
+  TBuf8<1024> logBuf;
+  TTime localTime; RThread thread;
+  localTime.HomeTime();
+  logBuf.Format(_L8("0x%08.8d|%02.2d:%02.2d:%02.2d.%06.6d: %S: 0x%x\n"),thread.Id(),localTime.DateTime().Hour(),localTime.DateTime().Minute(),localTime.DateTime().Second(),localTime.DateTime().MicroSecond(),&aBuffer,aParam);
+  file.Write(logBuf);
+  file.Close();
+  fs.Close();
+}
+
+static void Log(const TDesC& aBuffer,TInt aParam)
+{
+  TBuf8<1024> cnvBuf;
+  cnvBuf.Copy(aBuffer);
+  Log(cnvBuf,aParam);
+}*/
+
+
+
+_LIT(KAlarmSound,"a");
+
+CClkAlmView* CClkAlmView::NewLC(CClkAlmModel* anAlm,CClkNitzModel* aNitz)
+{
+  CClkAlmView* self=new(ELeave)CClkAlmView;
+  CleanupStack::PushL(self);
+  self->ConstructL(anAlm,aNitz);
+  return self;
+}
+
+CClkAlmView::~CClkAlmView()
+{
+  delete iControl;
+  delete iAlmObserver;
+  delete iNitzObserver;
+}
+
+TBool CClkAlmView::OpenedByOk(void) const
+{
+  return iOpenedByOk;
+}
+
+void CClkAlmView::SetOpenedByOk(TBool aOpenedByOk)
+{
+  iOpenedByOk=aOpenedByOk;
+}
+
+void CClkAlmView::HandleSettingsChangeL(TClkEnvChanged anEvent)
+{
+  if(iControl)
+  {
+    UpdateAlarmStateL(1);
+    UpdateNitzStateL(1);
+    iControl->DrawNow();
+  }
+}
+
+TBool CClkAlmView::UpdateNitzStateL(TInt aParam)
+{
+  TInt state;
+  User::LeaveIfError(iNitz->NitzState(state));
+  if(!aParam)
+  {
+    if(iNitzSettings==state) return EFalse;
+  }
+  iNitzSettings=state;
+  iControl->SetNitzInfoL();
+  return EFalse;
+}
+
+CClkNitzModel* CClkAlmView::NitzMdl(void)
+{
+  return iNitz;
+}
+
+TInt CClkAlmView::NitzState(void)
+{
+  return iNitzSettings;
+}
+
+TUid CClkAlmView::Id(void) const
+{
+  return KClkAlmViewId;
+}
+
+void CClkAlmView::HandleCommandL(TInt aCommand)
+{
+  switch(aCommand)
+  {
+    case 0x6000:
+    case 0x6001:
+      QueryAlarmTimeL();
+      break;
+    case 0x6002:
+      HandleDeleteCmdL(0,0);
+      break;
+    case 0x6003:
+      iClkAppUi->CmdSettingsL();
+      break;
+    case 0x6004:
+      iClkAppUi->CmdHelpL();
+      break;
+    case EAknSoftkeyExit:
+    case 0x6005:
+      iClkAppUi->CmdExit();
+      break;
+  }
+}
+
+void CClkAlmView::DynInitMenuPaneL(TInt aResourceId,CEikMenuPane* aMenuPane)
+{
+  if(aResourceId==0x10931009)
+  {
+    if(IsSnoozedAlarm()||iAlm->ClockAlarmState(0)==1)
+    {
+      aMenuPane->DeleteMenuItem(0x6000);
+    }
+    else
+    {
+      aMenuPane->DeleteMenuItem(0x6001);
+      aMenuPane->DeleteMenuItem(0x6002);
+    }
+    if(OpenedByOk())
+    {
+      aMenuPane->DeleteMenuItem(0x6004);
+      aMenuPane->DeleteMenuItem(0x6005);
+      SetOpenedByOk(EFalse);
+    }
+  }
+}
+
+void CClkAlmView::HandleUpdateL(TInt aNotification)
+{
+  if(aNotification!=KErrServerTerminated)
+  {
+    TBool res1=UpdateAlarmStateL(0),res2=UpdateNitzStateL(0);
+    if(res1||res2)
+    {
+      if(iControl) iControl->DrawNow();
+    }
+  }
+}
+
+void CClkAlmView::DoActivateL(const TVwsViewId& aPrevViewId,TUid aCustomMessageId,const TDesC8& aCustomMessage)
+{
+  User::LeaveIfError(iAlm->ClockAlarmInfo(0,iAlarmInfo));
+  iSnoozed=IsSnoozedAlarm();
+  User::LeaveIfError(iNitz->NitzState(iNitzSettings));
+  if(!iAlmObserver)
+  {
+    iAlmObserver=CClkAlmIdleObserver::NewL(this,-16);
+    iAlm->SetClkModelObserver(iAlmObserver);
+  }
+  if(!iNitzObserver)
+  {
+    iNitzObserver=CClkNitzIdleObserver::NewL(this,-5);
+    iNitz->SetClkModelObserver(iNitzObserver);
+  }
+  if(!iControl)
+  {
+    iControl=CClkAlmViewContainer::NewL(ClientRect(),*this);
+    iControl->SetMopParent(this);
+    iClkAppUi->AddToStackL(*this,iControl);
+  }
+  ShowTitlePaneL();
+  UpdateAlarmStateL(1);
+  UpdateNitzStateL(1);
+  iControl->ActivateL();
+}
+
+void CClkAlmView::DoDeactivate(void)
+{
+  if(iAlmObserver)
+  {
+    iAlm->SetClkModelObserver(NULL);
+    delete iAlmObserver;
+    iAlmObserver=NULL;
+  }
+  if(iNitzObserver)
+  {
+    iNitz->SetClkModelObserver(NULL);
+    delete iNitzObserver;
+    iNitzObserver=NULL;
+  }
+  if(iControl)
+  {
+    iClkAppUi->RemoveFromStack(iControl);
+    delete iControl;
+  }
+  iControl=NULL;
+}
+
+void CClkAlmView::ConstructL(CClkAlmModel* anAlm,CClkNitzModel* aNitz)
+{
+  BaseConstructL(0x10931007);
+  iAlm=anAlm;
+  iNitz=aNitz;
+}
+
+void CClkAlmView::ShowTitlePaneL(void)
+{
+  iClkAppUi->ShowTitlePaneL(0x10931004); //FIXME
+}
+
+TBool CClkAlmView::UpdateAlarmStateL(TInt aParam)
+{
+  SClockAlarmInfo info;
+  iSnoozed=IsSnoozedAlarm();
+  User::LeaveIfError(iAlm->ClockAlarmInfo(iSnoozed?1:0,info));
+  if(!aParam&&iAlarmInfo.iState==info.iState)
+  {
+    if(!(iAlarmInfo.iAlarmTime!=info.iAlarmTime)) return EFalse;
+  }
+  iAlarmInfo=info;
+  if(iSnoozed)
+  {
+    iControl->ShowSnoozedAlarmL(iAlarmInfo.iAlarmTime);
+  }
+  else
+  {
+    if(iAlarmInfo.iState==EAlarmSet)
+    {
+      iControl->ShowActiveAlarmL(iAlarmInfo.iAlarmTime);
+    }
+    else
+    {
+      iControl->ShowInactiveAlarm();
+    }
+  }
+  return ETrue;
+}
+
+void CClkAlmView::QueryAlarmTimeL(void) //ok
+{
+  CClkDocument* document=(CClkDocument*)(AppUi()->Document());
+  TTime alarm,alarmOld;
+  if(iAlarmInfo.iState==EAlarmSet)
+  {
+    alarm=iAlarmInfo.iAlarmTime;
+  }
+  else
+  {
+    alarm=document->GetLastAlarmTime();
+  }
+  alarmOld=alarm;
+  CAknTimeQueryDialog* dialog=new(ELeave)CAknTimeQueryDialog(alarm/*,tone*/);
+  if(dialog->ExecuteLD(0x10931010))
+  {
+    if(iSnoozed)
+    {
+      TInt changed=EFalse;
+      if(alarmOld!=alarm) changed=ETrue;
+      HandleDeleteCmdL(1,changed);
+    }
+    SClockAlarmInfo info;
+    info.iAlarmTime=alarm;
+    info.iRepeat=EAlarmClockNext24hours;
+    info.iSound.Copy(KAlarmSound);
+    iAlm->ClockAlarmSet(0,info);
+    User::LeaveIfError(iAlm->ClockAlarmEnable(0,EAlarmSet));
+    document->SetLastAlarmTime(alarm);
+    TRAPD(err,document->StoreDataL());
+    ShowRemainTimeL(info.iAlarmTime);
+  }
+}
+
+void CClkAlmView::HandleDeleteCmdL(TInt aParam1,TInt aParam2)
+{
+  if(aParam1==0)
+  {
+    CAknQueryDialog* dialog=CAknQueryDialog::NewL();
+    if(!dialog->ExecuteLD(0x1093100F)) return;
+  }
+  TInt alarmId=iSnoozed;
+  if(alarmId) alarmId=1;
+  if(iAlm->ClockAlarmState(alarmId)==EAlarmSet)
+  {
+    User::LeaveIfError(iAlm->ClockAlarmEnable(alarmId,EAlarmDisabled));
+  }
+  if(iSnoozed)
+  {
+    iSnoozed=EFalse;
+    if(aParam2)
+    {
+      UpdateAlarmStateL(1);
+      if(iControl) iControl->DrawNow();
+    }
+  }
+}
+
+void CClkAlmView::ShowRemainTimeL(const TTime& anAlarmTime)
+{
+  TTime time;
+  time.HomeTime();
+  TDateTime timeS=time.DateTime();
+  TDateTime alarmTimeS=anAlarmTime.DateTime();
+  alarmTimeS.SetYear(timeS.Year());
+  alarmTimeS.SetMonth(timeS.Month());
+  alarmTimeS.SetDay(timeS.Day());
+  TTime time2(alarmTimeS);
+  if(time2<time) time2+=TTimeIntervalDays(1);
+  TInt minutes=(time2.MicroSecondsFrom(time).Int64()/TInt64(60000000)).GetTInt();
+  CArrayFixFlat<TInt>* array=new(ELeave)CArrayFixFlat<TInt>(2);
+  CleanupStack::PushL(array);
+  TInt hours=minutes/60;
+  minutes=minutes-hours*60;
+  array->AppendL(hours);
+  array->AppendL(minutes);
+
+  //FIXME: fix this stupid code
+  HBufC* string;
+  if(hours==1&&minutes==1) string=StringLoader::LoadLC(0x10931011,*array,iCoeEnv);
+  else if(hours==1&&minutes!=1) string=StringLoader::LoadLC(0x10931013,*array,iCoeEnv);
+  else if(hours!=1&&minutes==1) string=StringLoader::LoadLC(0x10931014,*array,iCoeEnv);
+  else string=StringLoader::LoadLC(0x10931012,*array,iCoeEnv);
+/*  if(hours==1)
+  {
+    if(minutes==1) string=StringLoader::LoadLC(0x10931011,*array,iCoeEnv); //FIXME
+    else string=StringLoader::LoadLC(0x10931013,*array,iCoeEnv); //FIXME
+  }
+  else
+  {
+    if(minutes==1) string=StringLoader::LoadLC(0x10931014,*array,iCoeEnv); //FIXME
+    else string=StringLoader::LoadLC(0x10931012,*array,iCoeEnv); //FIXME
+  }*/
+  // string=StringLoader::LoadLC(((hours==1)?((minutes==1)?0x10931011:0x10931013):((minutes==1)?0x10931014:0x10931012)),*array,iCoeEnv); //FIXME
+  CAknConfirmationNote* note=new(ELeave)CAknConfirmationNote;
+  note->ExecuteLD(*string);
+  CleanupStack::PopAndDestroy(2); //string,array
+}
+
+TBool CClkAlmView::IsSnoozedAlarm(void)
+{
+  TAlarmSetState alm0=iAlm->ClockAlarmState(0),alm1=iAlm->ClockAlarmState(1);
+  TBool res=EFalse;
+  if(alm0!=EAlarmSet&&alm1==EAlarmSet) res=ETrue;
+  return res;
+}
