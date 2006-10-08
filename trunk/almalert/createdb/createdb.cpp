3304b36c932f5bbc79f7111116c514b9ac776059
z-g <z-g@28b348bd-d910-0410-8a2e-dc5dcabf243e>
2006-10-08 12:07:19 +0000

    createdb application added.

---
+++ trunk/almalert/createdb/createdb.cpp
@@ -0,0 +1,120 @@
+/*
+    createdb.cpp
+    Copyright (C) 2006 zg
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+*/
+
+#include <e32base.h>
+#include <e32cons.h>
+#include <f32file.h>
+#include <d32dbms.h>
+
+_LIT(KPanic,"createdb");
+
+_LIT(KDbName,"e:\\almalert.db");
+
+_LIT(KFieldId,"id");
+_LIT(KFieldName,"name");
+_LIT(KFieldValue,"value");
+
+_LIT(KIndex,"name");
+
+_LIT(KIntTable,"integers");
+_LIT(KStringTable,"strings");
+_LIT(KDateTable,"dates");
+
+const TInt KNameLength=32;
+
+LOCAL_C void DoMainL(void)
+{
+  RFs fs;
+  User::LeaveIfError(fs.Connect());
+  CleanupClosePushL(fs);
+  RDbNamedDatabase db;
+  if(db.Create(fs,KDbName)==KErrNone)
+  {
+    CleanupClosePushL(db);
+    //создаются таблицы
+    CDbColSet* columns=CDbColSet::NewLC();
+
+    TDbCol id(KFieldId,EDbColUint32);
+    id.iAttributes=TDbCol::ENotNull|TDbCol::EAutoIncrement;
+    columns->AddL(id);
+    {
+      TDbCol name(KFieldName,EDbColText16,KNameLength);
+      name.iAttributes=TDbCol::ENotNull;
+      columns->AddL(name);
+      TDbCol value(KFieldValue,EDbColInt32);
+      value.iAttributes=TDbCol::ENotNull;
+      columns->AddL(value);
+      User::LeaveIfError(db.CreateTable(KIntTable,*columns));
+    }
+    columns->Clear();
+    columns->AddL(id);
+    {
+      TDbCol name(KFieldName,EDbColText16,KNameLength);
+      name.iAttributes=TDbCol::ENotNull;
+      columns->AddL(name);
+      TDbCol value(KFieldValue,EDbColLongText16);
+      value.iAttributes=TDbCol::ENotNull;
+      columns->AddL(value);
+      User::LeaveIfError(db.CreateTable(KStringTable,*columns));
+    }
+    columns->Clear();
+    columns->AddL(id);
+    {
+      TDbCol name(KFieldName,EDbColText16,KNameLength);
+      name.iAttributes=TDbCol::ENotNull;
+      columns->AddL(name);
+      TDbCol value(KFieldValue,EDbColDateTime);
+      value.iAttributes=TDbCol::ENotNull;
+      columns->AddL(value);
+      User::LeaveIfError(db.CreateTable(KDateTable,*columns));
+    }
+    CleanupStack::PopAndDestroy(); //columns
+    //создаются индексы
+    CDbKey* key=CDbKey::NewLC();
+    {
+      TDbKeyCol name(KFieldName);
+      key->AddL(name);
+      key->MakeUnique();
+      User::LeaveIfError(db.CreateIndex(KIndex,KIntTable,*key));
+      User::LeaveIfError(db.CreateIndex(KIndex,KStringTable,*key));
+      User::LeaveIfError(db.CreateIndex(KIndex,KDateTable,*key));
+    }
+    CleanupStack::PopAndDestroy(); //key
+    CleanupStack::PopAndDestroy(); //db
+  }
+  CleanupStack::PopAndDestroy(); //fs
+}
+
+LOCAL_C void MainL(void)
+{
+  CActiveScheduler* scheduler=new(ELeave)CActiveScheduler;
+  CleanupStack::PushL(scheduler);
+  CActiveScheduler::Install(scheduler);
+  TRAPD(err,DoMainL());
+  if(err!=KErrNone) User::Panic(KPanic,err);
+  CleanupStack::PopAndDestroy(); //scheduler
+}
+
+GLDEF_C TInt E32Main(void)
+{
+  CTrapCleanup* cleanup=CTrapCleanup::New();
+  TRAPD(error,MainL());
+  delete cleanup;
+  return 0;
+}
