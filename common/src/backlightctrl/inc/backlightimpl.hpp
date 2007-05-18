ac2ed2d648f2486e84a72876f682a7a9fc87d126
z-g <z-g@28b348bd-d910-0410-8a2e-dc5dcabf243e>
2007-05-18 09:10:36 +0000

    backlightctrl rewrited.

--- common/src/backlightctrl/inc/backlightimpl.hpp
+++ common/src/backlightctrl/inc/backlightimpl.hpp
@@ -3,8 +3,11 @@
 
 #include <backlightctrl.hpp>
 #include <coemain.h>
+#include <phonet.hpp>
+
 #include "backlighttimer.hpp"
 
+class CIsiMsg;
 class CBackLightControlImpl: public CBackLightControl,public MCoeForegroundObserver,public MTimeOutNotify
 {
   public:
@@ -70,6 +73,37 @@ class CBackLightControlImpl: public CBackLightControl,public MCoeForegroundObser
     SBlink iKeysTime;
     SBlink iScreenCurrentTime;
     SBlink iKeysCurrentTime;
+  private:
+    void Open(void);
+    void Close(void);
+    void SetGameModeL(TBool aState);
+    void SwitchL(TInt aType,TInt aState);
+    void SetBrightnessL(TUint8 aValue);
+    void BrightnessL(TUint8& aValue);
+    CIsiMsg* SendAndReceiveLC(CIsiMsg* aSend);
+    void SendL(CIsiMsg* aSend);
+  private:
+    enum TSwitchType
+    {
+      ESwitchScreen=1,
+      ESwitchKeys=2,
+      ESwitchBoth=3
+    };
+    enum TSwitchState
+    {
+      ESmoothOn=1,
+      ESmoothOff=2,
+      EBlink=3,
+      EBlinkAlways=4,
+      EOn=5,
+      EUnlock=0xa1,
+      ELock=0xa2
+    };
+  private:
+    TBool iCaptured;
+    RMutex iMutex;
+    RPhoNet iPhoNet;
+    TUint8 iObjectNumber;
 };
 
 #endif
