eec824326abdf906debc369ffe3a0cd6a3dbcb5b
z-g <z-g@28b348bd-d910-0410-8a2e-dc5dcabf243e>
2007-06-15 13:31:08 +0000

    MPinbEntryChangedCallBack added.

---
+++ trunk/common/inc/PinbCallBack.hpp
@@ -0,0 +1,31 @@
+/*
+    PinbCallBack.hpp
+    Copyright (C) 2007 zg
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+*/
+
+#ifndef __PINBCALLBACK_HPP__
+#define __PINBCALLBACK_HPP__
+
+#include <f32file.h>
+
+class MPinbEntryChangedCallBack
+{
+  public:
+    virtual void UpdateEntryL(const TDriveUnit aDrive)=0;
+};
+
+#endif
