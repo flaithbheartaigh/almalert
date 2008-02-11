d5638d2a8ca0c99f18e280456ad4a7268b2439d6
z-g <z-g@28b348bd-d910-0410-8a2e-dc5dcabf243e>
2008-02-11 01:59:19 +0000

    bteng.hpp updated.

--- trunk/common/inc/BtEng.hpp
+++ trunk/common/inc/BtEng.hpp
@@ -22,6 +22,10 @@
 
 #include <CommonEngine.hpp>
 #include <bttypes.h>
+#include <btdevice.h>
+#include <btsdp.h>
+#include <es_sock.h>
+#include <btmanclient.h>
 
 enum TBTDiscoverabilityMode
 {
@@ -87,7 +91,109 @@ class CBTMCMSettings: public CBase,public MSharedDataNotifyHandler
 class RBTDeviceHandler
 {
   public:
+    IMPORT_C TInt AddDeviceIntoRegistryL( CBTDevice& aDevice );
+    IMPORT_C TInt GetDevicesFromRegistryL( CBTDeviceArray& aDevices );
+    IMPORT_C TInt DeleteDeviceFromRegistryL( CBTDevice& aDevice );
+    IMPORT_C TInt ModifyDeviceInRegistryL( CBTDevice& aDevice );
+    IMPORT_C TInt SetDeviceSecurityL(CBTDevice& aDevice,TBool aAuthenticate,TBool aAuthorise,TBool aEncrypt);
+    IMPORT_C TInt RemoveTrustFromDevicesL(void);
     IMPORT_C TInt DeleteDevicesWithoutLinkKeyL(void);
+    IMPORT_C TInt DeleteAllDevicesL(void);
+    IMPORT_C TInt IsDeviceConnectedL(const CBTDevice& aDevice,TBool& aStatus);
+    IMPORT_C TInt AreExistingConnectionsL(TBool& aStatus);
+    IMPORT_C TInt CloseBasebandConnectionL(const CBTDevice& aDevice);
+    IMPORT_C TInt CloseAllBasebandConnectionsL(void);
+    IMPORT_C TInt GetOpenBasebandConnectionsL(CBTDeviceArray& aDevices);
+  private:
+    TInt GetBTAddressArrayL(TUint& aLinkCount,TBTDevAddr*& aLinkArray);
+    void AddDeviceIntoListL(CBTDevice* aDevice,CBTDeviceArray* aDeviceArray);
+};
+
+class MBTSdpSearchComplete
+{
+  public:
+    virtual void ReturnChannel(TUint aChannel,TInt aStatus)=0;
+    virtual void SearchComplete(TInt aStatus)=0;
+};
+
+class MBTDeviceSearchObserver
+{
+  public:
+    virtual void DeviceSearchComplete(TInt aStatus)=0;
+};
+
+class MBTBondingObserver
+{
+  public:
+    virtual void BondingCompleteL(TInt aStatus)=0;
+};
+
+class CBTBondingHandler;
+class CBTSdpDbHandler;
+class CBTSdpResultReceiver;
+
+class CBTConnection: public CBase,public MBTSdpSearchComplete,public MBTDeviceSearchObserver
+{
+  public:
+    IMPORT_C static CBTConnection* NewL(void);
+    IMPORT_C static CBTConnection* NewLC(void);
+    ~CBTConnection();
+    IMPORT_C TInt SearchRemoteDeviceL(CBTDevice& aDevice,TBTMajorServiceClass aServiceClass);
+    IMPORT_C TInt SearchRemoteDeviceL(CBTDevice& aDevice);
+    IMPORT_C TInt SearchRemoteChannelL(const CBTDevice& aDevice,TUint aService,TUint& aChannel);
+    IMPORT_C TInt SearchRemoteChannelL(const CBTDevice& aDevice,TUint aService,TUint& aChannel,TRequestStatus& aStatus);
+    IMPORT_C TInt RegisterSecuritySettingsL(TUint aService,TInt aProtocol,TInt aChannel,TBool aAuthenticate,TBool aAuthorise,TBool aEncrypt);
+    IMPORT_C TInt UnregisterSecuritySettingsL(TUint aService);
+    IMPORT_C TInt RegisterSDPSettingsL(TUint aService,TUint aChannel,TSdpServRecordHandle& aHandle);
+    IMPORT_C TInt UnregisterSDPSettingsL(TSdpServRecordHandle aHandle);
+    IMPORT_C TInt ReserveLocalChannelL(RSocketServ& aSocketServer,RSocket& aListenSocket,TUint& aChannel);
+    IMPORT_C TInt ReleaseLocalChannelL(RSocket& aListenSocket);
+    IMPORT_C TInt PassiveConnectSocketL(RSocketServ& aSocketServer,RSocket& aDataSocket,RSocket& aListenSocket,TUint& aChannel,TRequestStatus& aStatus);
+    IMPORT_C TInt ActiveConnectSocketL(RSocketServ& aSocketServer,RSocket& aSocket,CBTDevice& aDevice,TUint aChannel,TRequestStatus& aStatus);
+    IMPORT_C TInt ActiveConnectSocketL(RSocketServ& aSocketServer,RSocket& aSocket,TInt aProtocol,CBTDevice& aDevice,TUint aChannel,TRequestStatus& aStatus);
+    IMPORT_C TInt GetRemoteDeviceInfoL(RSocket& aSocket,CBTDevice& aDevice);
+    IMPORT_C TInt DisconnectSocketL(RSocket& asocket,RSocket::TShutdown aHow=RSocket::ENormal);
+    IMPORT_C void AuthenticateConnection(RSocket& aSocket,TRequestStatus& aStatus);
+    IMPORT_C void CancelAuthentication(RSocket& aSocket);
+    IMPORT_C void CancelConnection(RSocket& aSocket);
+    IMPORT_C TInt PerformBondingL(CBTDevice& aDevice,MBTBondingObserver* aBondingObserver);
+    IMPORT_C void CancelBonding(void);
+  private: //MBTSDPSearchComplete
+    void ReturnChannel(TUint aChannel,TInt aStatus);
+    void SearchComplete(TInt aStatus);
+  private: //MBTDeviceSearchObserver
+    void DeviceSearchComplete(TInt aStatus);
+  private:
+    TSdpServRecordHandle RegisterSDPRecordDUNL(TUint8 aChannel);
+    TSdpServRecordHandle RegisterSDPRecordFaxL(TUint8 aChannel);
+    TSdpServRecordHandle RegisterSDPRecordObjectPushL(TUint8 aChannel);
+    TSdpServRecordHandle RegisterSDPRecordFileTransferL(TUint8 aChannel);
+  private:
+    void ConstructL(void);
+    CBTConnection();
+  private:
+    CBTBondingHandler* iBondingHandler;
+    CBTSdpDbHandler* iBTSdpDbHandler;
+    RBTMan iBTMan;
+    RBTSecuritySettings iSecSet;
+    CBTSdpResultReceiver* iSdpResultReceiver;
+    CSdpAgent* iSdpAgent;
+    CSdpSearchPattern* iSdpSearchPattern;
+    TUint iChannel;
+    TInt iStatus;
+    TInt iDeviceSearchStatus;
+};
+
+class CBTRfs: public CBase
+{
+  public:
+    IMPORT_C static CBTRfs* NewL(void);
+    IMPORT_C static CBTRfs* NewLC(void);
+    ~CBTRfs();
+    IMPORT_C TInt RestoreFactorySettingsL(TBool aUser=ETrue);
+  private:
+    void ConstructL(void);
+    CBTRfs();
 };
 
 #endif
